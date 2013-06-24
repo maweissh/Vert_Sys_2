862d0e39409070e807cf544e4c4d7483c6d5b92c
olwaibel <o.waibel@gmx.de>
2013-06-24 16:15:29 +0200

    klj

---
+++ VertSys_Aufgabe_3/src/groupwareAnwendung/Server.java
@@ -0,0 +1,17 @@
+package groupwareAnwendung;
+
+import rm.chat.user.UserAdministration;
+
+public class Server {
+	
+	private Listener listener;
+
+
+	
+	private UserAdministration useradministration;
+	
+	public Server(int i){
+		
+	}
+
+}
