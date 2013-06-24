bb7afa6339681d433e457be126a88819936f37b5
olwaibel <o.waibel@gmx.de>
2013-06-24 15:15:11 +0200

    blubb

--- VertSys_Aufgabe_3/src/groupwareAnwendung/ServerMain.java
+++ VertSys_Aufgabe_3/src/groupwareAnwendung/ServerMain.java
@@ -3,12 +3,15 @@ package groupwareAnwendung;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import rm.chat.user.Group;
+import rm.chat.user.User;
+
 public class ServerMain {
 	public static void main(String[] args) {
-		User eins = new User(null, "UserEins", "A", true);
-		User zwei = new User(eins, "UserZwei", "b", false);
+		User eins = new User("UserEins", "pw1", true);
+		User zwei = new User("UserZwei", "pw2", false);
 
-		Group gruppeeins = new Group(zwei);
+		Group gruppeeins = new Group("GruppeEins", zwei);
 
 		System.out.println(gruppeeins.getGroupname());
 		System.out.println(gruppeeins.getOwner());
